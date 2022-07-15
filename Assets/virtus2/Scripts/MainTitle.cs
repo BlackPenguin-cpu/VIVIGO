db0e5938fac05dcaffaced2e177fb4bbff19abea
virtus2 <khd1323@naver.com>
2022-07-16 04:57:44 +0900

    메인 타이틀 추가

---
+++ Assets/virtus2/Scripts/MainTitle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainTitle : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+
+    }
+
+    public void OnClickExitButton()
+    {
+
+    }
+}
