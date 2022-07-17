b45e18976858ce69a1246ce5c868a2cb41d8174c
virtus2 <khd1323@naver.com>
2022-07-17 19:34:54 +0900

    구현: 재시작 R키, 스킵 T키

---
+++ Assets/ManagerScripts/InputManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : SingletonManager<InputManager>
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Restart
+            // TODO: 게임오버한셈 치고 재시작함(야매)
+            // TODO: 나중에 바꿔야할 수도 있을까
+            GameManager.Instance.GameOver();
+        }
+
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            // Skip
+            // TODO: 클리어한것으로 치고 다음으로 넘어감(야매)
+            // TODO: 나중에 바꿔야할 수도 있을까
+            GameManager.Instance.PlayerReachedGoal();
+        }
+    }
+}
