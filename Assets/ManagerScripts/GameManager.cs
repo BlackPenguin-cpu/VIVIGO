df0157c2f16ab0bb4e032bea737c7eae48292189
Black_Penguin <dkstmdwnswns@gmail.com>
2022-07-17 18:22:53 +0900

    arrowKeyMaxCount 7 and some effect

--- Assets/ManagerScripts/GameManager.cs
+++ Assets/ManagerScripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using DG.Tweening;
 public class GameManager : SingletonManager<GameManager>
 {
     public GameObject playerPrefab;
@@ -19,6 +21,7 @@ public class GameManager : SingletonManager<GameManager>
 
     public ArrowKeyManager arrowKeyManager;
 
+    public PostProcessVolume volume;
     private void Awake()
     {
         base.Awake();
@@ -55,22 +58,25 @@ public class GameManager : SingletonManager<GameManager>
     public void PlayerReachedGoal()
     {
         Debug.Log("플레이어 목표 타일 도달");
-        GameStage.Instance.NextStage();
+        StartCoroutine(ClearEffect());
         // TODO: 스테이지또는 적 오브젝트를 Destroy?
-        for (int i = 0; i < enemies.Count; i++)
-        {
-            Destroy(enemies[i].gameObject);
-        }
-        Destroy(player.gameObject);
-        enemies.Clear();
     }
     IEnumerator ClearEffect()
     {
-        while (true)
+        float value = 0;
+        volume.profile.TryGetSettings(out Vignette vignette);
+        Camera.main.transform.DOMove(FindObjectOfType<Player>().transform.position + new Vector3(0, 0, -10), 1).SetEase(Ease.InCirc);
+        while (value < 1)
         {
-
+            vignette.intensity.value = value;
+            value += Time.deltaTime;
             yield return null;
         }
+        yield return new WaitForSeconds(1);
+        vignette.intensity.value = 0;
+        GameReset();
+        enemies.Clear();
+        GameStage.Instance.NextStage();
     }
     /// <summary>
     /// 게임 초기화시 사용
--- Assets/ManagerScripts/SingletonManager.cs
+++ Assets/ManagerScripts/SingletonManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SingletonManager<T> : MonoBehaviour where T : MonoBehaviour
 {
     public static T Instance;
